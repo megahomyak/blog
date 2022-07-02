10a54a22f4fa0204ed3e1a5afe3777728c899e4b
megahomyak <g.megahomyak@gmail.com>
2022-07-02 13:53:42 +0500

    Better log level setting and error message

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::{path::Path, sync::Arc};
+use std::{path::Path, str::FromStr, sync::Arc};
 
 use simple_logger::SimpleLogger;
 
@@ -12,24 +12,21 @@ impl FileNameShortcut for Path {
     }
 }
 
-pub fn set_global_log_level(log_level_name: impl AsRef<str>) -> Result<(), &'static str> {
-    let log_level_filter = match &log_level_name.as_ref().to_lowercase()[..] {
-        "off" => log::LevelFilter::Off,
-        "error" => log::LevelFilter::Error,
-        "warn" => log::LevelFilter::Warn,
-        "info" => log::LevelFilter::Info,
-        "debug" => log::LevelFilter::Debug,
-        "trace" => log::LevelFilter::Trace,
-        _ => {
-            return Err(
-                r#"Log level's lowercase representation isn't in \
-                ["off", "error", "warn", "info", "debug", "trace"]!"#
+pub fn set_global_log_level(log_level_name: impl AsRef<str>) -> Result<(), String> {
+    if let Ok(log_level_filter) = log::LevelFilter::from_str(log_level_name.as_ref()) {
+        SimpleLogger::new()
+            .with_level(log_level_filter)
+            .init()
+            .unwrap();
+        Ok(())
+    } else {
+        Err(format!(
+            "Log level not in [{}]!",
+            itertools::Itertools::intersperse(
+                log::LevelFilter::iter().map(|variant| variant.as_str()),
+                ", "
             )
-        }
-    };
-    SimpleLogger::new()
-        .with_level(log_level_filter)
-        .init()
-        .unwrap();
-    Ok(())
+            .collect::<String>()
+        ))
+    }
 }
